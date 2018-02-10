b1d671b61201fb3c9b3ab8740ac3f32f25bf1fd9
Michael Weinberg <web@mweinberg.com>
2018-02-10 00:37:42 -0500

    initial modifications

--- tweet_dumper.py
+++ tweet_dumper.py
@@ -1,14 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
+#requires python 3.6 and Tweepy
+#rename config-sample.yml to config.yml and add your Twitter credentials
+
 
 import tweepy #https://github.com/tweepy/tweepy
 import csv
+import time
+import os
+import sys
+
+
+import yaml
+
+def get_script_path():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
+config_location = os.path.join(get_script_path(),'config.yml')
+
+with open(config_location, 'r') as ymlfile:
+    config = yaml.load(ymlfile)
+
+
+consumer_key = config['consumer_key']
+consumer_secret = config['consumer_secret']
+access_key = config['access_key']
+access_secret = config['access_secret']
 
 #Twitter API credentials
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
+
+
+
 
 
 def get_all_tweets(screen_name):
@@ -23,7 +45,7 @@ def get_all_tweets(screen_name):
 	alltweets = []	
 	
 	#make initial request for most recent tweets (200 is the maximum allowed count)
-	new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+	new_tweets = api.user_timeline(screen_name = screen_name,count=200, tweet_mode='extended')
 	
 	#save most recent tweets
 	alltweets.extend(new_tweets)
@@ -33,10 +55,10 @@ def get_all_tweets(screen_name):
 	
 	#keep grabbing tweets until there are no tweets left to grab
 	while len(new_tweets) > 0:
-		print "getting tweets before %s" % (oldest)
+		print ("getting tweets before %s" % (oldest))
 		
 		#all subsiquent requests use the max_id param to prevent duplicates
-		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+		new_tweets = api.user_timeline(screen_name = screen_name,count=200, tweet_mode='extended', max_id=oldest)
 		
 		#save most recent tweets
 		alltweets.extend(new_tweets)
@@ -44,13 +66,14 @@ def get_all_tweets(screen_name):
 		#update the id of the oldest tweet less one
 		oldest = alltweets[-1].id - 1
 		
-		print "...%s tweets downloaded so far" % (len(alltweets))
+		print ("...%s tweets downloaded so far" % (len(alltweets)))
 	
 	#transform the tweepy tweets into a 2D array that will populate the csv	
-	outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+	outtweets = [[tweet.id_str, tweet.created_at, tweet.full_text.encode("utf-8")] for tweet in alltweets]
 	
+	now = time.strftime("%Y-%m-%d-%H-%M-%S%Z")
 	#write the csv	
-	with open('%s_tweets.csv' % screen_name, 'wb') as f:
+	with open(f'{screen_name}_tweets_{now}.csv', 'w') as f:
 		writer = csv.writer(f)
 		writer.writerow(["id","created_at","text"])
 		writer.writerows(outtweets)
@@ -59,5 +82,8 @@ def get_all_tweets(screen_name):
 
 
 if __name__ == '__main__':
-	#pass in the username of the account you want to download
-	get_all_tweets("J_tsar")
+	if len(sys.argv) == 2:
+		get_all_tweets(sys.argv[1])
+	else:
+		print("Please enter screen name to download.")
+		
